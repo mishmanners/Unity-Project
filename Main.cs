9b5614445d2a0a44e9375eb4c02de12d1008fccb
MishManners <36594527+MishManners@users.noreply.github.com>
2020-04-17 22:57:30 +1000

    Step 2
    Change main camera and add log files

---
+++ Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if space key is pressed --> print message
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("space key");
+        }
+        // if e --> print
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Press E");
+        }       
+    }
+}
