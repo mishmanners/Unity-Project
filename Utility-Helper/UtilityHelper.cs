abfac1beb917c350adeda2d0966c800c8ab3bca8
MishManners <36594527+MishManners@users.noreply.github.com>
2020-05-19 19:00:10 +1000

    Add files via upload

---
+++ Utility-Helper/UtilityHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UtilityHelper
+{
+    public static void CreateObject()
+    {
+        //Create new primitive object (ie. cube)
+
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+    }
+
+    public static void SetOrigPosition(GameObject obj)
+    {
+        //change the position of the object
+
+        obj.transform.position = Vector3.zero;
+
+    }
+    
+
+
+}
