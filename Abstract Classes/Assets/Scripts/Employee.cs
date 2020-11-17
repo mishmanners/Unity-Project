1a08bd5d254ad2e74fc43b44ef0451492738742a
MishManners <mishmanners@github.com>
2020-11-18 00:22:35 +1100

    Abstract Classes
    Added

---
+++ Abstract Classes/Assets/Scripts/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Employee : MonoBehaviour
+{
+    public int employeeName;
+    public int companyName;
+}
+
+public class FullTime : Employee
+{
+    CalculateMonthlySalary();
+}
+
+public class PartTime : Employee
+{
+    CalculateMonthlySalary();
+}
---
+++ Abstract Classes/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public int speed;
+    public int health;
+    public int gems;
+
+    public abstract void Attack();
+    public virtual void Die()
+    {
+        Destroy(this.gameObject);
+    }
+    
+
+}
+
+public class CrimsonTwilight : Enemy
+{
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Die()
+    {
+        //custom particles
+        base.Die();
+    }
+
+}
